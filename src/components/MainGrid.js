a8508947bd0cb1349742382e3e5d07f6f949600c
AliWisam <aliwisaam73@gmail.com>
2022-01-17 01:51:55 +0500

    new fix

--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -5,7 +5,7 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-//importing MainGrid
+// importing MainGrid
 import GlobalData from "./GlobalData";
 import LocalData from "./LocalData";
 import Chart from "./Chart";
